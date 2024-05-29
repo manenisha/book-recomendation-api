0e1ca7e9eb861e256c7b8e15952b646e9db6e9a2
manenisha <nishamane2@gmail.com>
2024-05-29 19:22:58 +0530

    updated config files

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    GOOGLE_API_KEY: "AIzaSyC9a22T5jVZQ6qF727WCbXaTji21nRuVZo"
+    GOOGLE_API_KEY: ""
 };
--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "sql12.freesqldatabase.com",
-    USER: "sql12709908",
-    PASSWORD: "VsCEC88GQs",
-    DB: "sql12709908",
+    HOST: "",
+    USER: "",
+    PASSWORD: "",
+    DB: "",
     dialect: "mysql",
     pool: {
         max: 5,
